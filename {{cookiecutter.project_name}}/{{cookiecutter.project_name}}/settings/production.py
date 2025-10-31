2f5ba080ee00ce41081b21767b1817fb9942b57d
Szymon Cader <szymon.sc.cader@gmail.com>
2025-10-31 16:00:03 +0100

    Make sentry and django extensions mandatory

--- {{cookiecutter.project_name}}/{{cookiecutter.project_name}}/settings/production.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.project_name}}/settings/production.py
@@ -1,7 +1,5 @@
-{% if cookiecutter.use_sentry == "y" -%}
 import sentry_sdk
 from sentry_sdk.integrations.django import DjangoIntegration
-{%- endif %}
 
 from . import env
 from .base import *
@@ -10,12 +8,10 @@ DEBUG = False
 
 ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", ["*"])
 
-{%- if cookiecutter.use_sentry == "y" %}
 sentry_sdk.init(
-    dsn="",
+    dsn=env("SENTRY_DSN", ""),
     integrations=[DjangoIntegration()],
 )
-{%- endif %}
 
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 
